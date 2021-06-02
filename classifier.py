f0468f1cc5fc2702db3891e678e55f9ce6ebfd59
morV <mor.testa96@gmail.com>
2021-06-02 21:11:26 +0300

    this my first commit

--- classifier.py
+++ classifier.py
@@ -3,7 +3,7 @@
 crimes_dict = {0: 'BATTERY', 1: 'THEFT', 2: 'CRIMINAL DAMAGE', 3: 'DECEPTIVE PRACTICE', 4: 'ASSAULT'}
 
 def predict(X):
-    pass
+    print("hi guys")
 
 def send_police_cars(X):
     pass
